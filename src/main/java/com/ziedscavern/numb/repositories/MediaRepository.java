a8f3a52f2d196b33359ceddc3b926a178b4347ed
ZiedYousfi <yousfi_zied@yahoo.com>
2025-10-03 13:15:22 +0200

    Add query methods to MediaRepository for title and type lookup

--- src/main/java/com/ziedscavern/numb/repositories/MediaRepository.java
+++ src/main/java/com/ziedscavern/numb/repositories/MediaRepository.java
@@ -1,8 +1,15 @@
 package com.ziedscavern.numb.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
 import com.ziedscavern.numb.domain.Media;
 
 public interface MediaRepository extends MongoRepository<Media, String> {
 
+  @Query("{ 'title': ?0 }")
+  Media findByTitle(String title);
+
+  @Query("{ 'type': ?0 }")
+  Media findByType(String type);
 }
