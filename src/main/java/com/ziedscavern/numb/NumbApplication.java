284d5c956ad18f963a78859541e69ba5b6d41335
ZiedYousfi <yousfi_zied@yahoo.com>
2025-10-03 11:19:12 +0200

    Initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/main/java/com/ziedscavern/numb/NumbApplication.java
@@ -0,0 +1,13 @@
+package com.ziedscavern.numb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ziedscavern/numb/NumbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziedscavern.numb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
